f8e40642b536c003936fa60f830a3ce464bd9a63
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-06-03 21:48:21 +0530

    js in jsx with curly braces

--- 01_React-Basic/src/TItle.jsx
+++ 01_React-Basic/src/TItle.jsx
@@ -1,6 +1,11 @@
 function Title() {
+  let msg = "Siddhant Nagaria";
   return (
-    <h1>I am Title !</h1>
+    <>
+      <h1>I am Title !</h1>
+      <h3> 2 * 2 = {2 * 2}</h3>
+      {msg}
+    </>
   )
 }
 
