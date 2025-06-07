8fcb0a2fb9d6073d396bb0cbcb6fb6a542f6ee03
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-06-07 22:23:03 +0530

    passing arrays to props

--- 01_React-Basic/src/Product.jsx
+++ 01_React-Basic/src/Product.jsx
@@ -1,8 +1,10 @@
-function Product({ title, price = 1 }) {
+function Product({ title, price = 1, features = [] }) {
     return (
         <div className="Product">
             <h3>{title}</h3>
             <h5> Price: {price}</h5>
+            <p>{features}</p>
+            <p>{features[0]}</p>
             <hr />
         </div>
     );
--- 01_React-Basic/src/ProductTab.jsx
+++ 01_React-Basic/src/ProductTab.jsx
@@ -1,9 +1,11 @@
 import Product from "./Product";
 
 function ProductTab() {
+    let features = ["hi-tech", 'durable', "fast"];
+    // let options = { a: "hi-tech", b: "durable", c: "fast" };
     return (
         <>
-            <Product title="phone" price={30000} />
+            <Product title="phone" price={30000} features={features} />
             <Product title="Laptop" price={80000} />
             <Product title="Pen" /> {/* price is automatically set to 1 from Product.jsx*/}
         </>
