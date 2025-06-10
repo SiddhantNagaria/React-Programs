8c83f0ceddbd1899c089e016bbe3a7b43f2bd56d
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-06-10 13:57:37 +0530

    component styling

--- 01_React-Basic/src/Product.jsx
+++ 01_React-Basic/src/Product.jsx
@@ -3,9 +3,10 @@ function Product({ title, price = 1, features = [] }) {
         <li key={index}>{feature}</li>
     ));
     let isDiscount = price >20000 ? "Discount of 5%" : "";
+    let styles = {backgroundColor : price >20000 ? "pink" : ""};
     if (price > 20000) {
         return (
-            <div className="Product">
+            <div className="Product" style={styles}>
                 <h3>{title}</h3>
                 <h5> Price: {price}</h5>
                 <p>{isDiscount}</p>
