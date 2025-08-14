ba5471e1c358db07e3a4ce559f13685d43f3a009
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-08-14 22:04:25 +0530

    non click events

--- 04_Events_in_React/src/Button.jsx
+++ 04_Events_in_React/src/Button.jsx
@@ -6,6 +6,14 @@ function printBye() {
     console.log("Bye");
 }
 
+function mouseOver(){
+    console.log("mouse over para");
+}
+
+function dblclick(){
+    console.log("you doubled click the button");
+}
+
 export default function Button() {
     return (
         <div>
@@ -13,6 +21,9 @@ export default function Button() {
                 click me
             </button>
             <p onClick={printBye}>Lorem ipsum dolor sit amet.</p>
+
+            <p onMouseOver={mouseOver}>Lorem ipsum dolor sit amet.</p>
+            <button onDoubleClick={dblclick}>double click me</button>
         </div>
     )
 }
